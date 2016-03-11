771d8e781bce2a8a950f186a91cec2c9e043834b
David Hayes and Nayshawn Danner <pair+dhayes+nayshawn@pivotal.io>
2016-03-11 08:33:18 -0600

    check for new pipeline status every 5 sec

--- reload-go.js
+++ reload-go.js
@@ -2,8 +2,17 @@ function loadLatestPipelineValueStreamMapPage(){
   var pipelineName = window.location.pathname.match("value_stream_map/(.*)/.*")[1];
   var pathWithoutId = window.location.pathname.match("(.*value_stream_map/.*/).*")[1];
 
-  jQuery.get("http://localhost:8153/go/api/pipelines/"+ pipelineName + "/history").then(function(data){
-     window.location.pathname =  pathWithoutId + data.pagination.total
+  var gocdHost = window.location.host;
+  jQuery.get("http://" + gocdHost + "/go/api/pipelines/"+ pipelineName + "/history").then(function(data){
+     var currentPipelineId = window.location.pathname.match(".*value_stream_map/.*/(.*)")[1];
+     if(currentPipelineId == null){
+       return;
+     }
+     var latestPipelineRunId = data.pagination.total;
+     if(latestPipelineRunId > parseInt(currentPipelineId)){
+        window.location.pathname =  pathWithoutId + data.pagination.total
+     }
   });
 }
-window.setInterval(loadLatestPipelineValueStreamMapPage, 30000);
+
+window.setInterval(loadLatestPipelineValueStreamMapPage, 5000);
